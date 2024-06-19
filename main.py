f1a2804d11a581ffe13d8159a166ea3bf20c2254
Vikramaditya <vikramaditya.@Vikramadityas-MacBook-Air.local>
2024-06-20 02:34:39 +0530

    first-commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from dotenv import load_dotenv
+load_dotenv() ## loading all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+## function to load Gemini Pro model and get repsonses
+model=genai.GenerativeModel("gemini-pro") 
+chat = model.start_chat()
+def get_gemini_response(question):
+    
+    response=chat.send_message(question,stream=True)
+    return response
+
+##initialize our streamlit app
+
+st.set_page_config(page_title="Q&A Demo")
+
+st.header("Gemini LLM Application")
+
+
+input=st.text_input("Input: ",key="input")
+submit=st.button("Ask the question")
+
+if submit and input:
+    response=get_gemini_response(input)
+    # Add user query and response to session state chat history
+    st.subheader("The Response is")
+    for chunk in response:
+        st.write(chunk.text)
+    
